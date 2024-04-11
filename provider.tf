1797ab354627cc1cafeefa1e659c7dbfd5582ae0
ajaysimha28 <166675503+ajaysimha28@users.noreply.github.com>
2024-04-10 22:26:09 -0400

    provider file uploaded

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+  version = "3.96.0"
+  client_id       = "c125946a-8484-4700-b0b5-b970d201820d"
+  client_secret   = "vKC8Q~8OC6CeTyvnec3h57eF_KBmUrNPtGjdba-6"
+  tenant_id       = "6636311a-5cfc-4cc7-80d7-913ba535ba5b"
+  subscription_id = "0f2b7d65-2b33-4124-b8bb-f6f8611829c3"
+}
